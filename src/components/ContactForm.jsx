7aa8d44f12406f6066360f707ab9297c57902abe
Caleb Hodges <chodgesdev@gmail.com>
2024-05-03 14:32:27 -0500

    Implement contact functionality

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,19 +1,27 @@
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import classes from "./ContactForm.module.css";
+
+import emailjs from '@emailjs/browser'
 
 import logo from "../pages/assetts/logo-look-left.gif";
 import Button from "./Button";
 
+
 const ContactForm = (props) => {
   const { show } = props;
   const formRef = useRef(null);
-  const scrollPointRef = useRef(null)
+  const scrollPointRef = useRef(null);
 
-  const { innerWidth: screenWidth } = window
-    console.log(screenWidth)
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [subject, setSubject] = useState();
+  const [message, setMessage] = useState();
+
+
+  const { innerWidth: screenWidth } = window;
   useEffect(() => {
-    scrollPointRef.current.scrollIntoView()
-  })
+    scrollPointRef.current.scrollIntoView();
+  });
 
   const handleClick = (e) => {
     //Check for click outside the form to exit
@@ -29,22 +37,57 @@ const ContactForm = (props) => {
     return () => document.removeEventListener("mousedown", handleClick);
   });
 
+  const submitMessage = (e) => {
+    e.preventDefault();
+   const templateParams = {
+    name,
+    email,
+    subject,
+    message
+  };
+
+  console.log(process.env.REACT_APP_EMAIL_PUBKEY)
+
+  emailjs
+    .send(
+      `${process.env.REACT_APP_EMAIL_SERVICE}`,
+      `${process.env.REACT_APP_EMAIL_TEMPLATE}`,
+      templateParams,
+      `${process.env.REACT_APP_EMAIL_PUBKEY}`
+    )
+    .then(() => console.log("Sent"))
+    .catch((err) => console.log(err));
+  };
+
   return (
     <div className={classes.dimBackground} ref={scrollPointRef}>
       <div className={classes.formWrapper} ref={formRef}>
         <form className={classes.form}>
-            <h1 style={{textDecoration: "underline"}}>Email Me</h1>
-          <input type="text" placeholder="Name" />
-          <input type="text" placeholder="Email" />
-          <input type="text" placeholder="Subject" />
+          <h1 style={{ textDecoration: "underline" }}>Email Me</h1>
+          <input
+            type="text"
+            placeholder="Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Subject"
+            onChange={(e) => setSubject(e.target.value)}
+          />
           <textarea
             name="message"
             id="message"
             placeholder="Message"
             cols="30"
             rows="10"
+            onChange={(e) => setMessage(e.target.value)}
           ></textarea>
-          <Button title="Send" />
+          <Button title="Send" onClick={submitMessage} />
         </form>
         <div className={classes.logoWrapper}>
           <img src={logo} alt="" />
